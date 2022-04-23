17592cf1dd5719338e19146e1d10f6afdc8d3b2b
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-04-23 12:56:29 +0100

    set windows save/restore geometry

--- roe/include/window.h
+++ roe/include/window.h
@@ -2,13 +2,26 @@
 #define WINDOW_H
 
 #include <QMainWindow>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QAction>
+#include <QIcon>
+#include <QSettings>
+#include <QPixmap>
+#include <QCloseEvent>
 
-class Window : public QMainWindow
-{
+class Window : public QMainWindow {
     Q_OBJECT
 
 public:
     Window(QWidget *parent = nullptr);
     ~Window();
+    void readSettings();
+    void closeEvent(QCloseEvent *event);
+public:
+    QVBoxLayout *layout;
+    QVBoxLayout *ctlayout;
+    QWidget *centralwidget;
+
 };
 #endif // WINDOW_H
--- roe/src/window.cc
+++ roe/src/window.cc
@@ -1,11 +1,34 @@
 #include "window.h"
 
 Window::Window(QWidget *parent)
-    : QMainWindow(parent)
-{
+    : QMainWindow(parent) {
+
+    readSettings();
+    layout = new QVBoxLayout();
+    ctlayout = new QVBoxLayout();
+    ctlayout->setContentsMargins(0,0,0,0);
+
+    centralwidget = new QWidget();
+    centralwidget->setStyleSheet("background-color: #bdbdbd;");
+    setCentralWidget(centralwidget);
+
+    this->setLayout(layout);
+
+    centralwidget->setLayout(ctlayout);
+
 }
 
-Window::~Window()
-{
+void Window::readSettings() {
+    auto settings = new QSettings("XIO", "Roe");
+    auto _geometry = settings->value("geometry");
+    this->restoreGeometry(_geometry.toByteArray());
 }
+
+void Window::closeEvent(QCloseEvent *event){
+    auto settings = new QSettings("XIO", "Roe");
+    settings->setValue("geometry", this->saveGeometry());
+}
+
+
+Window::~Window() {}
 
