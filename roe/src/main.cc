071e309871ee6abe40b3b4b2649ba06c326100a1
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-04-23 12:29:21 +0100

    Set application name.

--- roe/src/main.cc
+++ roe/src/main.cc
@@ -1,9 +1,15 @@
 #include "window.h"
 
 #include <QApplication>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setApplicationVersion("1.0");
+    QCoreApplication::setOrganizationDomain("org.roe.xio");
+    QCoreApplication::setOrganizationName("XIO");
+    QCoreApplication::setApplicationName("Roe");
+
     QApplication a(argc, argv);
     Window w;
     w.show();
